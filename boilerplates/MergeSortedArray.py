80dd2e121ed346c626e6e7f09ad51c2b4ce374af
Akhand Singh <akhans@amazon.com>
2020-11-09 18:59:42 -0800

    merge sorted arrays

--- boilerplates/MergeSortedArray.py
+++ boilerplates/MergeSortedArray.py
@@ -21,12 +21,34 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
+        result = [None] * (m + n)
+        i = j = k = 0
+        while i < m and j < n:
+            if nums1[i] <= nums2[j]:
+                result[k] = nums1[i]
+                k += 1
+                i += 1
+            else:
+                result[k] = nums2[j]
+                k += 1
+                j += 1
 
+        while i < m:
+            result[k] = nums1[i]
+            i += 1
+            k += 1
+
+        while j < n:
+            result[k] = nums2[j]
+            j += 1
+            k += 1
+
+        return result
 
 if __name__ == "__main__":
     s = Solution()
-    nums1 = [1, 2, 3, 0, 0, 0]
+    nums1 = [1, 2, 3]
     m = 3
     nums2 = [2, 5, 6]
     n = 3
-    s.merge(nums1, m, nums2, n)
+    print(s.merge(nums1, m, nums2, n))
